b88f603a5d26ae7329b4ca5b1d2fddf167ebe9d0
Carl Freyer <ckf225@lehigh.edu>
2025-01-27 19:28:42 -0500

    testing

--- src/main/java/com/example/carlfreyercom/CarlFreyerCom.java
+++ src/main/java/com/example/carlfreyercom/CarlFreyerCom.java
@@ -5,13 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class CarlFreyerCom {
 
   @Value("${NAME:World}")
   String name;
-
+  
+  @CrossOrigin(origins = "https://www.carlfreyer.com")
   @RestController
   class CarlFreyerComController {
     @GetMapping("/")
